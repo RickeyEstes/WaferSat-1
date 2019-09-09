72e57258a5e1d0994dd7cd74eac790ef8552577e
Rajan <rajansaini691@gmail.com>
2019-09-09 16:02:56 -0700

    Added keras2arm.py

---
+++ src/astrometry/keras2arm.py
@@ -0,0 +1,14 @@
+from keras.models import Model
+
+def keras2arm(model, filepath):
+    """
+    Acts as an interpreter between a keras model and the CMSIS neural network
+    library. 
+
+    @Param model        Reads from this model
+    @Param filepath     Writes the generated C Code to this filepath
+    """
+    
+
+    return None;
+
