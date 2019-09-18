91613d89c9faff5c1e1c27604ef89a720fcc07d1
Rajan <rajansaini691@gmail.com>
2019-09-18 16:10:04 -0700

    Better printing, fixed q format mistake

--- src/astrometry/keras2arm.py
+++ src/astrometry/keras2arm.py
@@ -31,7 +31,7 @@ class _LayerWrapper:
         self.layer = layer
 
         # Unique name assigned to layer
-        self.name = layer.name 
+        self.layer_name = layer.name.upper()
 
         # Converted and quantized weights, for ARM
         self.new_weights = []
@@ -40,13 +40,13 @@ class _LayerWrapper:
         # Number of bits per weight (either 8 or 16)
         self.weight_size = 8
 
-        # Number of decimal bits per image pixel
-        # TODO Is the input vector recasted as a series of q7 or q.7 integers?
+        # Number of decimal bits per image pixel (we recast from 0-255 to 0-1)
         # This may be dependent on the network architecture.
-        self.image_q_shift = 0
+        self.image_q_shift = 8
 
-        # Shift (calculated after quantization)
-        self.shift = 0
+        # Shifts for fixed point arithmetic (calculated after quantization)
+        self.bias_shift = 0
+        self.output_shift = 0
 
     def _has_weights(self):
         """
@@ -54,6 +54,12 @@ class _LayerWrapper:
         etc.), so it is worthwhile to check if the given layer does
         """
         return len(self.layer.get_weights()) > 0
+
+    def _is_convolution_layer(self):
+        return "conv" in self.layer.name
+
+    def _is_dense_layer(self):
+        return "dense" in self.layer.name
 
     def convert_weights(self):
         """
@@ -71,20 +77,18 @@ class _LayerWrapper:
         weights = params[0]
         bias = params[1]
 
-        if "dense" in self.layer.name:
+        if self._is_convolution_layer():
             # TODO Check if data format is some analogue of "channels first" 
             # before converting
 
             # Transpose layer weights to the (output, input) shape
             self.new_weights = weights.T
         
-        elif "conv" in self.layer.name:
+        elif self._is_dense_layer(): 
             # The layer's data format should be channels_last. If the trained
             # model is channels_first, we must reshape
-            # TODO Reshape instead of throwing an error 
-            assert("last" in self.layer.data_format, 
-                """ Currently the converter only accepts convolution layers 
-                    with the \"channels last\" data format """)
+            # TODO Check data format before blindly transposing
+            self.new_weights = weights.T
 
         # Flatten weights C-Style to export to a 1D array
         self.new_weights = weights.flatten('C')
@@ -114,16 +118,58 @@ class _LayerWrapper:
         # Use ARM's method to perform the quantization
         self.new_weights = np.round((2**dec_bits)*self.new_weights)
 
+        # Do the same for the bias
+        min_bias = min(self.new_bias)
+        max_bias = max(self.new_bias)
+        int_bias_bits = max(0, np.ceil(np.log2(max(abs(min_wt),abs(max_wt)))))
+        dec_bias_bits = self.weight_size - int_bias_bits - 1
+
+        # Use ARM's method to perform the quantization
+        self.new_bias = np.round((2**dec_bias_bits)*self.new_bias)
+
         # Calculating the shift is trivial if we assume the input vector
         # consists of 8-bit integers from (0-255), quantized to an unsigned 
-        # q7.0 or q8.0 format (TODO double-check if we get an extra bit if unsigned)
-        self.shift = self.image_q_shift + dec_bits
+        # UQ.8 format falling in [0,1)
+        self.bias_shift = self.image_q_shift + dec_bits - dec_bias_bits
+
+        # TODO This assumes the output precision should always be the same as
+        # the input vector. For deeper models this should depend on the needed
+        # precision of the next layer instead of being a fixed model.
+        self.output_shift = dec_bits
 
         # TODO Better model accuracy can be achieved if the model were
         # repeatedly evaluated during the quantization process. This will not
         # affect performance during inference, only memory usage at higher
         # resolutions. 
+        
+        return None
 
+    def _print_parameter(self, param_name, param_value):
+        """
+        Prints the given parameter as a preprocessor macro
+        
+        # Example
+        _print_parameter("VAL", 1) with layer name "LAYER" generates:
+        #define LAYER_VAL = 1
+
+        # Arguments
+            param_name: Parameter name (string)
+            param_value: Value of the given parameter (int)
+        """
+
+        print("#define {}_{} = {}".format(self.layer_name, param_name.upper(),
+            param_value))
+        return None
+
+    def _print_convolution_parameters(self):
+        """
+        @pre                Current layer is a convolution layer
+        """
+        assert self.layer.rank==2,"Converter currently only supports Conv2D layers"
+
+        self._print_parameter("KERNEL_X", layer.kernel_size[0])
+        self._print_parameter("KERNEL_Y", layer.kernel_size[1])
+        self._print_parameter("STRIDES", layer.strides[0])
         return None
 
     def print_converted_weights(self):
@@ -136,16 +182,21 @@ class _LayerWrapper:
             return None
 
         # TODO Added to make debugging easier; remove when done
-        self.new_weights = self.new_weights[:70]
-
-        # Generated macro name
-        weights_name = self.name.upper()
+        self.new_weights = self.new_weights[:30]
 
         # Comma-separated string storing the weights
         weights_str = ','.join(str(int(weight)) for weight in self.new_weights)
-        print("#define %s = {%s}" % (weights_name, weights_str))
+        bias_str = ','.join(str(int(bias)) for bias in self.new_bias)
 
-        # TODO Handle bias
+        # Printing the preprocessor macros
+        print("")
+        self._print_parameter("WEIGHTS", weights_str)
+        self._print_parameter("BIAS", bias_str)
+        self._print_parameter("BIAS_SHIFT", self.bias_shift)
+        self._print_parameter("OUTPUT_SHIFT", self.output_shift)
+
+        if self._is_convolution_layer():
+            self._print_convolution_parameters()
 
         return None
 
